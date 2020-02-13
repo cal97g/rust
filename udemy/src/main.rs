e6b26972972abbff0dcdab3093ca3782c6cd1b82
callam <callam@callamdelaney.me>
2020-02-13 23:20:16 +0000

    Option<T>, Some, None, unions

--- udemy/src/main.rs
+++ udemy/src/main.rs
@@ -101,9 +101,75 @@ enum FootballTeams {
 }
 
 
+// unions
+// use the same memory location to store different data types; seems almost completely pointless
+// considering how cheap memory is. One thing to note with when borrowing a union field is that
+// all fields of the union will technically be borrowed for the same lifetime.
+
+union MassiveSaving {
+    i: u8,
+    f: f32
+}
+
+fn unions() {
+    let mut my_union = MassiveSaving{i: 1};
+    my_union.i = 200;
+
+    let val = unsafe { my_union.i };
+    println!("Val {}", val);
+
+    // we can also pattern match against unions; though returning clojures as a result (which is a nice pattern)
+    // is hacky af. See: https://github.com/rust-lang/rust/issues/24036#issuecomment-89509870
+
+    fn match_u(x : MassiveSaving) {
+        unsafe {
+            match x {
+                MassiveSaving {i: 123} => {println!("123!!")}
+                MassiveSaving {i} => {println!("Integer!")},
+                MassiveSaving {f} => {println!("Float!")},
+            }
+        }
+    }
+    match_u(MassiveSaving{i: 84});
+}
+
+
+// Option<T> is an enum included with the standard library and prelude; that is to say that it's
+// without importing it. That goes for its constituents too, Some(T) or None.
+// Option<T> is defined as
+// enum Option<T> {
+//     Some(T),
+//     None
+// }
+// the <T> syntax is a generic type parameter. It means data of any type can be present in Some.
+// https://doc.rust-lang.org/book/ch06-01-defining-an-enum.html#the-option-enum-and-its-advantages-over-null-values
+
+// n order to have a value that can possibly be null, you must explicitly opt in by making the type of that value Option<T>.
+// Then, when you use that value, you are required to explicitly handle the case when the value is null.
+// https://doc.rust-lang.org/std/option/enum.Option.html
+fn an_option() {
+    let mut y: Option<u8> = Some(5);
+
+    fn process_y(y: Option<u8>) {
+        match y {
+            Some(y) => {println!("y is present and its value is {}", y)}
+            None => {println!("y is None!")}
+        }
+    }
+    
+    process_y(y);
+    y = None;
+    process_y(y);
+
+
+}
+
 fn main() {
     age_check(15);
     loops();
     match_case_statement("order_create");
     structures();
+    unions();
+    an_option();
+
 }
